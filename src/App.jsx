c8f9210254e923cd63859b4350238cb0ab3139f0
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-26 13:18:38 +0300

    Cities and countries list in the app from fake API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AppLayout from './pages/AppLayout'
@@ -6,16 +7,48 @@ import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
+import CityList from './components/CityList'
+import CountryList from './components/CountryList'
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function() {
+    async function fetchCities() {
+      try { 
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('There was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+
+        {/* nested routes between <Route></Route> */}
+        <Route path="app" element={<AppLayout />}>
+          {/* index route is the default child route if none of other routes matches */}
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+
+          <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
+          <Route path='form' element={<p>Form</p>} />
+        </Route>
         {/* 404 page for all not found url's/pages */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
