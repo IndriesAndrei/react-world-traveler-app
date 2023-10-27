39959f30f6f0052cf066ce8f5a99aa3d6902078b
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-27 14:09:37 +0300

    Add geolocation button to map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import styles from './Map.module.css'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button'
+import styles from './Map.module.css'
 
 export default function Map() {
-    // useNavigate() hook for changing the location
-    const navigate = useNavigate();
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const {cities} = useCities();
+    const {isLoading: isLoadingPosition, position: geolocationPosition, getPosition} = useGeolocation();
 
     // reading/writing search params with useSearchParams()
     // /app/cities/73930385?lat=38.727881642324164&lng=-9.140900099907554
     const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat'); 
-    const lng = searchParams.get('lng');
+    const mapLat = searchParams.get('lat'); 
+    const mapLng = searchParams.get('lng');
+
+    useEffect(function() {
+       if(mapLat && mapLng) setMapPosition([mapLat, mapLng])
+    }, [mapLat, mapLng]);
+
+    useEffect(function() {
+        if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    }, [geolocationPosition])
 
     return (
         <div className={styles.mapContainer}>
+           {!geolocationPosition && (<Button type='position' onClick={getPosition}>
+                {isLoadingPosition ? 'Loading...' : 'Use your position'}
+            </Button>)}
+            
             <MapContainer 
                 center={mapPosition} 
-                zoom={13} 
+                zoom={8} 
                 scrollWheelZoom={true} 
                 className={styles.map}
             >
@@ -35,7 +48,31 @@ export default function Map() {
                         </Popup>
                     </Marker>
                 ))}
+
+                <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>
         </div>
     )
+}
+
+// custom component 
+function ChangeCenter({position}) {
+    const map = useMap();
+    map.setView(position);
+
+    return null;
+}
+
+// custom component
+function DetectClick() {
+    // useNavigate() hook for changing the location
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: (e) => {
+            console.log(e); //ex:  latlng: LatLng {lat: 41.45919537950706, lng: 0.3900146484375}
+            navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+        }
+    });
 }
