267b645528cde06b1a3f6765f33c511167d81eac
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-11-03 17:17:20 +0200

    Optimization to CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,6 +1,6 @@
 // custom Context and Provider
 
-import { createContext, useEffect, useContext, useReducer } from "react";
+import { createContext, useEffect, useContext, useReducer, useCallback } from "react";
 const BASE_URL = 'http://localhost:8000';
 
 const CitiesContext = createContext();
@@ -77,7 +77,8 @@ function CitiesProvider({children}) {
         fetchCities();
     }, []);
 
-    async function getCity(id) {
+    // useCallback is used with functions
+    const getCity = useCallback(async function getCity(id) {
         if (Number(id) === currentCity.id) return;
 
         dispatch({type: "loading"});
@@ -88,7 +89,7 @@ function CitiesProvider({children}) {
         } catch {
             dispatch({type: 'rejected', payload: 'There was an error loading the city...'});
         } 
-    }
+    }, [currentCity.id]);
 
     async function createCity(newCity) {
         dispatch({type: "loading"});
@@ -120,14 +121,15 @@ function CitiesProvider({children}) {
     }
 
     // passing all of our needed functions and data to the Context
-    return <CitiesContext.Provider value={{
-        cities,
-        isLoading,
-        currentCity,
-        getCity,
-        createCity,
-        deleteCity,
-    }}>
+    return <CitiesContext.Provider 
+        value={{
+            cities,
+            isLoading,
+            currentCity,
+            getCity,
+            createCity,
+            deleteCity,
+        }}>
         {children}
     </CitiesContext.Provider>
 }
